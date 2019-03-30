7357b55beb03d0c9637ed244c1a3e0f65e0722fe
ice-frog <ice-frog@ice-frog>
2019-03-31 00:18:35 +0100

    commit

---
+++ src/main/java/job_queue/Job.java
@@ -0,0 +1,26 @@
+package job_queue;
+
+import java.util.Map;
+import java.util.concurrent.Future;
+
+public class Job implements ScanningJob {
+
+	@Override
+	public ScanType getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getQuery() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Future<Map<String, Integer>> initaiate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
